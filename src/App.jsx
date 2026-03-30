237ae63a909586ec683a3dd17f4994a59d67eea2
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2026-03-30 18:14:07 +0100

    feat: Add missing components to App layout for improved structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import Footer from "./component/common/Footer";
 import Navbar from "./component/common/Navbar";
+import Contact from "./component/HomePage/Contact";
 import Hero from "./component/HomePage/Hero";
+import OurGallery from "./component/HomePage/OurGallery";
+import OurServices from "./component/HomePage/OurServices";
+import OurStory from "./component/HomePage/OurStory";
+import Review from "./component/HomePage/Review";
+import WhyUs from "./component/HomePage/WhyUs";
 
 function App() {
     return (
         <>
             <Navbar />
             <Hero />
+            <OurServices />
+            <WhyUs />
+            <OurGallery />
+            <OurStory />
+            <Review />
+            <Contact />
+            <Footer />
         </>
     );
 }
